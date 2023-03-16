f0140c60b0291a4eaed0c395dd0bc949245f4c8c
Xavi <xavi@arnaus.net>
2023-03-16 07:51:40 +0100

    Remove old formatting commented code

--- src/lib/formater.py
+++ src/lib/formater.py
@@ -39,24 +39,3 @@ class Formater:
     
     def _format_status(self, content: str, message_type: MessageType) -> str:
         return content
-
-
-    # def _format_post(self, post: dict, origin: str, site_options: dict) -> str:
-
-    #     title = post["title"]
-    #     title_only_chars = re.sub("^[A-Za-z]*", "", title)
-    #     if title_only_chars == title_only_chars.upper():
-    #         title = " ".join([word.capitalize() for word in title.lower().split(" ")])
-    #     link = post["link"]
-    #     summary = post["summary"] + "\n\n" if "summary" in post and post["summary"] and post["summary"] != "" else ""
-    #     summary = ''.join(BeautifulSoup(summary, "html.parser").findAll(text=True))
-    #     summary = summary.replace("\n\n\n", "\n\n")
-    #     summary = re.sub("\s+", ' ', summary)
-    #     max_length = site_options["max_summary_length"] \
-    #         if "max_summary_length" in site_options and site_options["max_summary_length"] \
-    #             else self.MAX_SUMMARY_LENGTH
-    #     summary = (summary[:max_length] + '...') if len(summary) > max_length+3 else summary
-
-    #     text = f"{origin}:\n" if "show_name" in site_options and site_options["show_name"] else ""
-        
-    #     return f"{text}\t{title}\n\n{summary}\n\n{link}"
--- src/lib/system_info_templater.py
+++ src/lib/system_info_templater.py
@@ -108,24 +108,3 @@ class SystemInfoTemplater:
             i += 1
         f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
         return '%s %s' % (f, self.SUFFIXES[i])
-
-
-    # def _format_post(self, post: dict, origin: str, site_options: dict) -> str:
-
-    #     title = post["title"]
-    #     title_only_chars = re.sub("^[A-Za-z]*", "", title)
-    #     if title_only_chars == title_only_chars.upper():
-    #         title = " ".join([word.capitalize() for word in title.lower().split(" ")])
-    #     link = post["link"]
-    #     summary = post["summary"] + "\n\n" if "summary" in post and post["summary"] and post["summary"] != "" else ""
-    #     summary = ''.join(BeautifulSoup(summary, "html.parser").findAll(text=True))
-    #     summary = summary.replace("\n\n\n", "\n\n")
-    #     summary = re.sub("\s+", ' ', summary)
-    #     max_length = site_options["max_summary_length"] \
-    #         if "max_summary_length" in site_options and site_options["max_summary_length"] \
-    #             else self.MAX_SUMMARY_LENGTH
-    #     summary = (summary[:max_length] + '...') if len(summary) > max_length+3 else summary
-
-    #     text = f"{origin}:\n" if "show_name" in site_options and site_options["show_name"] else ""
-        
-    #     return f"{text}\t{title}\n\n{summary}\n\n{link}"

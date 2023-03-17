0428c76ddf10ecabd07085081739ad081e0429fc
Xavi <xavi@arnaus.net>
2023-03-17 08:16:36 +0100

    Make Remore Runner to send the data only if Dry Run is False

--- run_remote.py
+++ run_remote.py
@@ -24,13 +24,16 @@ class RunRemote:
         sys_data = self._collect_data()
 
         # Send the data
-        remote_url = self._config.get("app.service.remote_url")
-        self._logger.info("Sending sys_data away")
-        r = requests.post(f"{remote_url}/sysinfo", json={'sys_data': sys_data})
-        if r.status_code == 200:
-            self._logger.info("Request was successful")
+        if not self._config.get("run_control.dry_run"):
+            remote_url = self._config.get("app.service.remote_url")
+            self._logger.info("Sending sys_data away")
+            r = requests.post(f"{remote_url}/sysinfo", json={'sys_data': sys_data})
+            if r.status_code == 200:
+                self._logger.info("Request was successful")
+            else:
+                self._logger.warning(f"Request was unsuccessful: {r.status_code}")
         else:
-            self._logger.warning(f"Request was unsuccessful: {r.status_code}")
+            self._logger.info("Dry Run, not sent.")
 
         self._logger.info("End.")
 

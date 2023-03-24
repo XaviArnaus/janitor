f6d213fc7d3625607b1a8fc4aaf27bcd8324cec3
Xavi <xavi@arnaus.net>
2023-03-24 09:34:26 +0100

    Change structure of the metric file, adding metadata

--- src/lib/metric_stats.py
+++ src/lib/metric_stats.py
@@ -29,6 +29,9 @@ class MetricStats:
     }
 
     TIME_METRIC_LABEL = "time"
+    TIME_METRIC_FORMAT = "%H:%M"
+    METADATA_OBJECT = "metadata"
+    METRICS_OBJECT = "metrics"
 
 
     def __init__(self, config: Config, hostname: str, when: MetricStatsWhen = MetricStatsWhen.DAILY) -> None:
@@ -37,28 +40,29 @@ class MetricStats:
         self._hostname = hostname
         self._when = when
         self._stack = Storage(self.get_current_filename(hostname, when))
+        self._initialize(hostname, when)
     
     def append(self, system_info: dict, exceptions: list = []) -> None:
-        self._logger.debug(f"Appending SystemInfo dataset into the file for [{self._hostname}] and [{self._when}]")
-        # First save all the metrics
+        self._logger.debug(f"Appending SystemInfo dataset into the file for [{self._hostname}] and [{self._when}]") 
+        # Save all the metrics
         for metric_name, metric_value in system_info.items():
             if metric_name in exceptions:
                 continue
-            data = list(self._stack.get(metric_name, []))
+            data = list(self._stack.get(f"{self.METRICS_OBJECT}.{metric_name}", []))
             data.append(metric_value)
-            self._stack.set(metric_name, data)
+            self._stack.set(f"{self.METRICS_OBJECT}.{metric_name}", data)
         # And now save the current time to get a sense of when it happened
-        data = list(self._stack.get(self.TIME_METRIC_LABEL, []))
-        data.append(datetime.strftime(datetime.now(timezone.utc), "%H:%M"))
-        self._stack.set(self.TIME_METRIC_LABEL, data)
+        data = list(self._stack.get(f"{self.METRICS_OBJECT}.{self.TIME_METRIC_LABEL}", []))
+        data.append(datetime.strftime(datetime.now(timezone.utc), self.TIME_METRIC_FORMAT))
+        self._stack.set(f"{self.METRICS_OBJECT}.{self.TIME_METRIC_LABEL}", data)
         # Finally write back the file
         self._stack.write_file()
     
     def get_metric(self, metric_name: str) -> list:
-        return self._stack.get(metric_name, [])
+        return self._stack.get(f"{self.METRICS_OBJECT}.{metric_name}", [])
     
     def get_time(self) -> list:
-        return self.get_metric(self.TIME_METRIC_LABEL)
+        return self.get_metric(f"{self.METRICS_OBJECT}.{self.TIME_METRIC_LABEL}")
     
     def get_all(self) -> dict:
         return self._stack.get_all()
@@ -69,6 +73,17 @@ class MetricStats:
             who = who,
             when = datetime.strftime(datetime.now(timezone.utc), self.PARAMS_PER_WHEN[when]["datetime_format"])
         )
+    
+    def _initialize(self, hostname: str, when: MetricStatsWhen) -> None:
+        current_metadata = self._stack.get(self.METADATA_OBJECT, None)
+        if not current_metadata:
+            self._logger.debug(f"Setting the metadata in the stack file for [{hostname}] and [{when}]")
+            self._stack.set(self.METADATA_OBJECT, {
+                "hostname": hostname,
+                "when": datetime.strftime(datetime.now(timezone.utc), self.PARAMS_PER_WHEN[when]["datetime_format"])
+            })
+            self._stack.set(self.METRICS_OBJECT, {})
+            self._stack.write_file()
 
 # class MetricStatsGraph:
 

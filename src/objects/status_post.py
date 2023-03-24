00cd65018497356856933314f691f7c247c1187e
Xavi <xavi@arnaus.net>
2023-03-24 17:14:20 +0100

    Minor fixies, new StatusPost and related objects test

--- src/objects/status_post.py
+++ src/objects/status_post.py
@@ -3,6 +3,88 @@ from datetime import datetime
 from strenum import LowercaseStrEnum
 
 
+class StatusPost:
+    """
+    Object to manage the status posts to be published through the Mastodon API.
+    Mastodon.py wrapper version 1.8.0
+
+    Should support Pleroma variations
+    (status, in_reply_to_id=None, media_ids=None, sensitive=False, visibility=None, spoiler_text=None, language=None, idempotency_key=None, content_type=None, scheduled_at=None, poll=None, quote_id=None)
+    """
+
+    status: str = None
+    in_reply_to_id: int = None
+    media_ids: list[int] = None
+    sensitive: bool = None
+    visibility: StatusPostVisibility = None
+    spoiler_text: str = None
+    language: str = None
+    idempotency_key: str = None
+    content_type: StatusPostContentType = None
+    scheduled_at: datetime = None
+    poll: any = None    # Poll not supported. It should be here a Poll object
+    quote_id: int = None
+
+    def __init__(self,
+                 status: str = None, 
+                 in_reply_to_id: int = None, 
+                 media_ids: list[int] = None,
+                 sensitive: bool = None, 
+                 visibility: StatusPostVisibility = None, 
+                 spoiler_text: str = None, 
+                 language: str = None, 
+                 idempotency_key: str = None, 
+                 content_type: StatusPostContentType = None, 
+                 scheduled_at: datetime = None, 
+                 poll: any = None, 
+                 quote_id: int = None) -> None:
+        
+        self.status = status
+        self.in_reply_to_id = in_reply_to_id
+        self.media_ids = media_ids
+        self.sensitive = sensitive if sensitive is not None else False
+        self.visibility = visibility if visibility is not None else StatusPostVisibility.PUBLIC
+        self.spoiler_text = spoiler_text
+        self.language = language
+        self.idempotency_key = idempotency_key
+        self.content_type = content_type if content_type is not None else StatusPostContentType.PLAIN
+        self.scheduled_at = scheduled_at
+        self.poll = poll
+        self.quote_id = quote_id
+
+    def to_dict(self) -> dict:
+        return {
+            "status": self.status,
+            "in_reply_to_id": self.in_reply_to_id,
+            "media_ids": self.media_ids,
+            "sensitive": self.sensitive,
+            "visibility": self.visibility,
+            "spoiler_text": self.spoiler_text,
+            "language": self.language,
+            "idempotency_key": self.idempotency_key,
+            "content_type": self.content_type,
+            "scheduled_at": self.scheduled_at.timestamp() if self.scheduled_at is not None else None,
+            "poll": self.poll,
+            "quote_id": self.quote_id,
+        }
+    
+    def from_dict(status_post_dict: dict) -> StatusPost:
+        return StatusPost(
+            status_post_dict["status"] if "status" in status_post_dict else None,
+            status_post_dict["in_reply_to_id"] if "in_reply_to_id" in status_post_dict else None,
+            status_post_dict["media_ids"] if "media_ids" in status_post_dict else None,
+            status_post_dict["sensitive"] if "sensitive" in status_post_dict else None,
+            StatusPostVisibility.valid_or_raise(status_post_dict["visibility"]) if "visibility" in status_post_dict else None,
+            status_post_dict["spoiler_text"] if "spoiler_text" in status_post_dict else None,
+            status_post_dict["language"] if "language" in status_post_dict else None,
+            status_post_dict["idempotency_key"] if "idempotency_key" in status_post_dict else None,
+            StatusPostContentType.valid_or_raise(status_post_dict["content_type"]) if "content_type" in status_post_dict else None,
+            datetime.fromtimestamp(status_post_dict["scheduled_at"]) if "scheduled_at" in status_post_dict else None,
+            status_post_dict["poll"] if "poll" in status_post_dict else None,
+            status_post_dict["quote_id"] if "quote_id" in status_post_dict else None,
+        )
+
+
 class StatusPostVisibility(LowercaseStrEnum):
     """
     The visibility parameter is a string value and accepts any of: 
@@ -24,88 +106,6 @@ class StatusPostVisibility(LowercaseStrEnum):
             raise RuntimeError(f"Value [{value}] is not a valid StatusPostVisibility")
         
         return value
-
-
-class StatusPost:
-    """
-    Object to manage the status posts to be published through the Mastodon API.
-    Mastodon.py wrapper version 1.8.0
-
-    Should support Pleroma variations
-    (status, in_reply_to_id=None, media_ids=None, sensitive=False, visibility=None, spoiler_text=None, language=None, idempotency_key=None, content_type=None, scheduled_at=None, poll=None, quote_id=None)
-    """
-
-    status: str = None
-    in_reply_to_id: int = None
-    media_ids: list[int] = None
-    sensitive: bool = False
-    visibility: StatusPostVisibility = StatusPostVisibility.PUBLIC
-    spoiler_text: str = None
-    language: str = None
-    idempotency_key: str = None
-    content_type: StatusPostContentType = None
-    scheduled_at: datetime = None
-    poll: any = None    # Poll not supported. It should be here a Poll object
-    quote_id: int = None
-
-    def __init__(self,
-                 status: str = None, 
-                 in_reply_to_id: int = None, 
-                 media_ids: list[int] = None,
-                 sensitive: bool = False, 
-                 visibility: StatusPostVisibility = StatusPostVisibility.PUBLIC, 
-                 spoiler_text: str = None, 
-                 language: str = None, 
-                 idempotency_key: str = None, 
-                 content_type: StatusPostContentType = None, 
-                 scheduled_at: datetime = None, 
-                 poll: any = None, 
-                 quote_id: int = None) -> None:
-        
-        self.status = status
-        self.in_reply_to_id = in_reply_to_id
-        self.media_ids = media_ids
-        self.sensitive = sensitive
-        self.visibility = visibility
-        self.spoiler_text = spoiler_text
-        self.language = language
-        self.idempotency_key = idempotency_key
-        self.content_type = content_type
-        self.scheduled_at = scheduled_at
-        self.poll = poll
-        self.quote_id = quote_id
-
-    def to_dict(self) -> dict:
-        return {
-            "status": self.status,
-            "in_reply_to_id": self.in_reply_to_id,
-            "media_ids": self.media_ids,
-            "sensitive": self.sensitive,
-            "visibility": self.visibility,
-            "spoiler_text": self.spoiler_text,
-            "language": self.language,
-            "idempotency_key": self.idempotency_key,
-            "content_type": self.content_type,
-            "scheduled_at": self.scheduled_at.timestamp(),
-            "poll": self.poll,
-            "quote_id": self.quote_id,
-        }
-    
-    def from_dict(status_post_dict: dict) -> StatusPost:
-        return StatusPost(
-            status_post_dict["status"],
-            status_post_dict["in_reply_to_id"],
-            status_post_dict["media_ids"],
-            status_post_dict["sensitive"],
-            StatusPostVisibility.valid_or_raise(status_post_dict["visibility"]),
-            status_post_dict["spoiler_text"],
-            status_post_dict["language"],
-            status_post_dict["idempotency_key"],
-            StatusPostContentType.valid_or_raise(status_post_dict["content_type"]),
-            datetime.fromtimestamp(status_post_dict["scheduled_at"]),
-            status_post_dict["poll"],
-            status_post_dict["quote_id"],
-        )
 
 
 class StatusPostContentType(LowercaseStrEnum):

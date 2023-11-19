b6cf8de40510346d238b23064d4f89b6ec3d2962
Xavier <xavi@arnaus.net>
2023-11-19 07:18:55 +0100

    format

--- janitor/lib/publisher.py
+++ janitor/lib/publisher.py
@@ -43,9 +43,7 @@ class Publisher:
         self._media_storage = self._config.get("publisher.media_storage")
 
         self._mastodon = MastodonHelper.get_instance(
-            connection_params=self._connection_params,
-            logger=self._logger,
-            base_path=base_path
+            connection_params=self._connection_params, logger=self._logger, base_path=base_path
         )
 
     def text(self, content: str, summary: str = None, requeue_if_fails: bool = False) -> dict:
